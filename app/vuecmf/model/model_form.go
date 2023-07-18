2a3cbaa2db07a3265930e91ed8cf02906dc23079
vuecmf <tulihua2004@126.com>
2023-07-18 18:25:56 +0800

    admin表增加pid字段、model_form表增加placeholder字段

--- app/vuecmf/model/model_form.go
+++ app/vuecmf/model/model_form.go
@@ -15,6 +15,7 @@ type ModelForm struct {
 	ModelFieldId uint   `json:"model_field_id" form:"model_field_id" binding:"required" required_tips:"请选择" gorm:"column:model_field_id;size:32;uniqueIndex:unique_index;not null;default:0;comment:模型字段ID"`
 	Type         string `json:"type" form:"type" binding:"required" required_tips:"请选择" gorm:"column:type;size:32;not null;default:'';comment:表单控件类型"`
 	DefaultValue string `json:"default_value" form:"default_value"  gorm:"column:default_value;size:255;not null;default:'';comment:表单控件默认值"`
+	Placeholder  string `json:"placeholder" form:"placeholder"  gorm:"column:placeholder;size:255;not null;default:'';comment:表单提示信息"`
 	IsDisabled   uint16 `json:"is_disabled" form:"is_disabled"  gorm:"column:is_disabled;size:8;not null;default:20;comment:添加/编辑表单中是否禁用： 10=是，20=否"`
 	SortNum      uint   `json:"sort_num" form:"sort_num"  gorm:"column:sort_num;size:32;not null;default:0;comment:菜单的排列顺序(小在前)"`
 	Status       uint16 `json:"status" form:"status"  gorm:"column:status;size:8;not null;default:10;comment:状态：10=开启，20=禁用"`
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package vuecmf
 
-const Version = "2.2.3"
+const Version = "2.3.0"
